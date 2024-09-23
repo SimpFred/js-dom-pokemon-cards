74a795362d8eed688cd229cec87e9a8ee6eb6301
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-23 11:56:59 +0200

    Added a renderCards function to dynamically generate Pokemon cards from the file data.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
+function renderCards(data) {
+  data.forEach((pokemon) => {
+    const cards = document.querySelector(".cards");
+    const card = document.createElement("li");
+    card.className = "card";
 
-console.log(data);
+    const title = document.createElement("h2");
+    title.className = "card--title";
+    title.textContent = pokemon.name;
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    const img = document.createElement("img");
+    img.className = "card--img";
+    img.width = 256;
+    img.src = pokemon.sprites.other["official-artwork"].front_default;
+
+    const text = document.createElement("ul");
+    text.className = "card--text";
+    const stats = [
+      "hp",
+      "attack",
+      "defense",
+      "special-attack",
+      "special-defense",
+      "speed",
+    ];
+
+    stats.forEach((stat) => {
+      const li = document.createElement("li");
+      li.textContent = `${stat.toUpperCase()}: ${
+        pokemon.stats.find((s) => s.stat.name === stat).base_stat
+      }`;
+      text.appendChild(li);
+    });
+
+    card.appendChild(title);
+    card.appendChild(img);
+    card.appendChild(text);
+    cards.appendChild(card);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderCards(data);
+});
